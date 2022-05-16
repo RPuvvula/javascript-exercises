2bc0105f9ee43fc0fd589ffcbb46c6f47d06f675
RPuvvula <mailtopuvvula@gmail.com>
2022-05-16 14:56:42 +1200

    upto 9 exercises are done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const santisedString = RemovePunctuation(word).toLowerCase();
+    return santisedString === santisedString.split('').reverse().join('');
 };
+
+//remove punctuation and spaces as Palindromes dont count those
+function RemovePunctuation(str) {
+    return str.replaceAll(/[.,:!?]/g, '').replaceAll(' ', '');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
