b2dc158ec78de0ba607aeb206089fa9ff08a210e
RPuvvula <mailtopuvvula@gmail.com>
2022-05-26 15:12:55 +1200

    small refactor

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,19 +1,17 @@
-const uppercase = [65, 90];
-const lowercase = [97, 122];
-
 const caesar = function (word, shift) {
-    let finalOutput = '';
-
-    word.split('').forEach(letter => {
+    const uppercase = [65, 90];
+    const lowercase = [97, 122];
+    
+    return word.split('').map(letter => {
         if (!".,:!? ".includes(letter)) {
             let charCode = letter.charCodeAt();
             //1. are these upper case or lower
             const minRange = charCode >= uppercase[0] && charCode <= uppercase[1] ? uppercase[0] : lowercase[0];
             const maxRange = charCode >= uppercase[0] && charCode <= uppercase[1] ? uppercase[1] : lowercase[1];
-            
+
             //add shift
             let afterShift = shift > 0 ? charCode + shift : charCode - Math.abs(shift);
-            
+
             //now after shift, are we going out of range? by how much and start from the beginning oe ending whatever overflown
             do {
                 if (afterShift > maxRange) {
@@ -24,13 +22,12 @@ const caesar = function (word, shift) {
                 }
             } while (afterShift > maxRange || afterShift < minRange);
 
-            finalOutput += String.fromCharCode(afterShift);
+            return String.fromCharCode(afterShift);
         }
         else {
-            finalOutput += letter;
+            return letter;
         }
-    });
-    return finalOutput;
+    }).join('');
 };
 
 // Do not edit below this line
